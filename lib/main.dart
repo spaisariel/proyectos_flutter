b1b4083d48b5ef83cfe2e2822270d9ca26eb5170
spaisariel <spaisariel@gmail.com>
2020-10-02 14:23:33 -0300

    commit x

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:prueba3_git/screens/menu_screen.dart';
 
-void main() => runApp(MenuScreen());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MenuScreen(),
+    );
+  }
+}
 
 //prueba cambio
