11cc74aad9e4acb5f3837f73cf2e77236bf4a497
Mateo Schultheis <mateo_as95@hotmail.com>
2020-10-28 08:08:49 -0300

    UserScreen

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prueba3_git/style/theme.dart' as Style;
 
 class UserScreen extends StatefulWidget {
   @override
@@ -8,77 +9,125 @@ class UserScreen extends StatefulWidget {
 class _UserScreenState extends State<UserScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              leading: IconButton(
-                  icon: Icon(Icons.filter_1),
-                  onPressed: () {
-                    // Do something
-                  }),
-              expandedHeight: 220.0,
-              floating: true,
-              pinned: true,
-              snap: true,
-              elevation: 50,
-              backgroundColor: Colors.pink,
-              flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: Text('Title',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      )),
-                  background: Image.network(
-                    'https://images.pexels.com/photos/443356/pexels-photo-443356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                    fit: BoxFit.cover,
-                  )),
+    return Scaffold(
+        body: DefaultTabController(
+      length: 2,
+      child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 200.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: Text("Perfil - Usuario Generico",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        )),
+                    background: Image.network(
+                      "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+                      fit: BoxFit.cover,
+                    )),
+              ),
+            ];
+          },
+          body: ListView(padding: const EdgeInsets.all(8), children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Casa Central",
+                  style: TextStyle(fontSize: 25),
+                ),
+                SizedBox(height: 50),
+                Text(
+                  "Deposito 1",
+                  style: TextStyle(fontSize: 25),
+                ),
+              ],
             ),
-            SliverPadding(
-              padding: const EdgeInsets.symmetric(vertical: 25.0),
-              sliver: new SliverList(
-                  delegate: new SliverChildListDelegate(_buildList(50))),
+
+            SizedBox(height: 50),
+
+            ListTile(
+              title: Row(
+                children: [
+                  Icon(Icons.mail),
+                  Column(
+                    children: [
+                      Text("E-Mail",
+                          style: TextStyle(color: Style.Colors.secondColor)),
+                      Text("mailgenerico@hotmail.com",
+                          style: TextStyle(color: Style.Colors.titleColor)),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-    );
-  }
 
-  List _buildList(int count) {
-    List<Widget> listItems = List();
+            //SOLO A MODO DE PRUEBA, EN UN FUTURO QUEDARIA UN SOLO LIST LTILE
+            ListTile(
+              title: Row(
+                children: [
+                  Icon(Icons.phone),
+                  Column(
+                    children: [
+                      Text("Telefono",
+                          style: TextStyle(color: Style.Colors.secondColor)),
+                      Text("+54 343 6200000",
+                          style: TextStyle(color: Style.Colors.titleColor)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: Row(
+                children: [
+                  Icon(Icons.place),
+                  Column(
+                    children: [
+                      Text("Dirección",
+                          style: TextStyle(color: Style.Colors.secondColor)),
+                      Text("Siempre Viva 123",
+                          style: TextStyle(color: Style.Colors.titleColor)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
 
-    listItems.add(
-      Text(
-        'mailgenerico@hotmail.com',
-        style: new TextStyle(fontSize: 25.0),
-      ),
-    );
+            SizedBox(height: 50),
 
-    listItems.add(SizedBox(height: 25));
+            ButtonTheme(
+              buttonColor: Style.Colors.mainColor,
+              height: MediaQuery.of(context).size.height * 0.1,
+              minWidth: MediaQuery.of(context).size.width * 0.8,
+              child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                  onPressed: () {},
+                  child: Text('Cambiar de sucursal o deposito',
+                      style: TextStyle(color: Colors.white, fontSize: 20))),
+            ),
 
-    listItems.add(
-      Text(
-        'fsg@hotmail.com',
-        style: new TextStyle(fontSize: 25.0),
-      ),
-    );
+            SizedBox(height: 25),
 
-    listItems.add(SizedBox(height: 25));
-
-    listItems.add(
-      Text(
-        'jhgjnfmhfhmnf@hotmail.com',
-        style: new TextStyle(fontSize: 25.0),
-      ),
-    );
-
-    listItems.add(SizedBox(height: 25));
-
-    listItems.add(RaisedButton(onPressed: null));
-
-    return listItems;
+            ButtonTheme(
+              buttonColor: Style.Colors.mainColor,
+              height: MediaQuery.of(context).size.height * 0.1,
+              minWidth: MediaQuery.of(context).size.width * 0.8,
+              child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                  color: Style.Colors.cancelColor,
+                  onPressed: () {},
+                  child: Text('Cerrar sesión',
+                      style: TextStyle(color: Colors.white, fontSize: 20))),
+            ),
+          ])),
+    ));
   }
 }
